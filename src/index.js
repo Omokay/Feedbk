8b103c940b4ce56615c8068cc51e5a3eb2859543
Omokay <omoke.chuku@up-ng.com>
2021-05-07 16:34:31 +0100

    to be updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('../config/keys');
+
 const app = express();
 
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+    (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken)
+    console.log(refreshToken)
+    console.log(profile)
+    }));
+
+
+
 app.get('/', (req, res) => {
-    res.send({"Hi":"Omoke"})
+   res.send({'Hi':'Omoke'})
 });
 
 
-app.listen(5000);
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.listen(process.env.PORT || 5000);
