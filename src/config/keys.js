9922fab5d1cad487116fadf1bc08d7b9086d2dab
Omokay <omoke.chuku@up-ng.com>
2021-05-13 05:24:24 +0100

    update gitignore

---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientID: '1048712414157-l6q812ne5smppilrtd8rjaj1bd2chsfo.apps.googleusercontent.com',
+    googleClientSecret: 'pGn_aoC5eS6bUoJbeWdD0bd',
+    mongoURL: 'user@omoke.test'
+}
